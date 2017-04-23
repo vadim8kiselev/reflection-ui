602afd37e70d8d40e28746cac74670fe5cbcc69b
amakaroff <alekseymakarov1996@gmail.com>
2017-04-23 19:52:46 +0400

    Fix value in annotations

--- src/main/java/com/kiselev/reflection/ui/annotation/AnnotationsUtils.java
+++ src/main/java/com/kiselev/reflection/ui/annotation/AnnotationsUtils.java
@@ -1,10 +1,11 @@
 package com.kiselev.reflection.ui.annotation;
 
 import com.kiselev.reflection.ui.indent.IndentUtils;
-import sun.reflect.annotation.AnnotationType;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.*;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -63,12 +64,47 @@ public class AnnotationsUtils {
 
             for (Method method : methods) {
                 method.setAccessible(true);
-                map.put(method.getName(), method.invoke(annotation));
+
+                Object value = method.invoke(annotation);
+                String fullValue = value.toString();
+
+                if (value.getClass().isArray()) {
+                    List<String> listValue = new ArrayList<String>();
+                    for (Object arrayValue : getArrayValues(value)) {
+                        if (arrayValue.getClass().isEnum()) {
+                            listValue.add(getEnumValue(arrayValue));
+                        } else {
+                            listValue.add(arrayValue.toString());
+                        }
+                    }
+
+                    fullValue = "[" + String.join(", ", listValue) + "]";
+                }
+
+                if (value.getClass().isEnum()) {
+                    fullValue = getEnumValue(value);
+                }
+
+                map.put(method.getName(), fullValue);
             }
         } catch (Exception exception) {
             exception.printStackTrace();
         }
 
         return map;
+    }
+
+    private String getEnumValue(Object object) {
+        return object.getClass().getSimpleName() + "." + object;
+    }
+
+    private Object[] getArrayValues(Object object) {
+        int length = Array.getLength(object);
+        Object[] objects = new Object[length];
+
+        for (int i = 0; i < length; i++) {
+            objects[i] = (Array.get(object, i));
+        }
+        return objects;
     }
 }
