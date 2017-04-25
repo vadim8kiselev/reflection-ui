37150c2210563fcff4ff55b9c4a21b2a4cf894ed
amakaroff <alekseymakarov1996@gmail.com>
2017-04-25 17:04:10 +0400

    Fix display class name and constructor in class java.util.regex.Pattern$1

--- src/main/java/com/kiselev/reflection/ui/constructor/ConstructorUtils.java
+++ src/main/java/com/kiselev/reflection/ui/constructor/ConstructorUtils.java
@@ -6,6 +6,7 @@ import com.kiselev.reflection.ui.exception.ExceptionUtils;
 import com.kiselev.reflection.ui.generic.GenericsUtils;
 import com.kiselev.reflection.ui.indent.IndentUtils;
 import com.kiselev.reflection.ui.modifier.ModifiersUtils;
+import com.kiselev.reflection.ui.name.NameUtils;
 
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
@@ -39,7 +40,7 @@ public class ConstructorUtils {
 
         String generics = new GenericsUtils().getGenerics(constructor);
 
-        String constructorName = constructor.getDeclaringClass().getSimpleName();
+        String constructorName = new NameUtils().getTypeName(constructor.getDeclaringClass());
 
         String arguments = new ArgumentUtils().getArguments(constructor);
 
