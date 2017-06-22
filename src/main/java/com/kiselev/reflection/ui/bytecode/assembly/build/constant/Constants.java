9eac1c44fe3032d887fb0190099d17d1370aceda
amakaroff <alekseymakarov1996@gmail.com>
2017-06-22 21:01:22 +0400

    Add constant for manifest home

--- src/main/java/com/kiselev/reflection/ui/bytecode/assembly/build/constant/Constants.java
+++ src/main/java/com/kiselev/reflection/ui/bytecode/assembly/build/constant/Constants.java
@@ -1,34 +1,15 @@
 package com.kiselev.reflection.ui.bytecode.assembly.build.constant;
 
-import java.io.File;
-
 public class Constants {
 
-    public static class Flag {
+    public static class Folders {
 
-        public static final String JAR_FLAG = "-cfvm";
-
-        public static final String JAR_C_FLAG = "-C";
+        public static final String MANIFEST_HOME = "META-INF";
     }
 
     public static class Properties {
 
         public static final String HOME_DIR = "user.dir";
-    }
-
-    public static class Command {
-
-        public static final String JAR_EXE_COMMAND = "\""
-                + System.getProperty("java.home")
-                + (!isJava9() ? File.separator + ".." : "")
-                + File.separator + "bin"
-                + File.separator + "jar.exe"
-                + "\"";
-
-        private static boolean isJava9() {
-            return System.getProperty("java.version").startsWith("9")
-                    || System.getProperty("java.version").startsWith("1.9");
-        }
     }
 
     public static class Suffix {
@@ -43,5 +24,9 @@ public class Constants {
     public static class Symbols {
 
         public static final String GAP = " ";
+
+        public static final String SLASH = "/";
+
+        public static final String POINT = ".";
     }
 }
