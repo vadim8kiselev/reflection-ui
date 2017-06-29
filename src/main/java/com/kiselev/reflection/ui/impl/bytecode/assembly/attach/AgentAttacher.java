f4342c72bb50aafb8b2eafd654e7d47029522c1f
amakaroff <alekseymakarov1996@gmail.com>
2017-06-29 17:58:35 +0400

    Delete imports

--- src/main/java/com/kiselev/reflection/ui/impl/bytecode/assembly/attach/AgentAttacher.java
+++ src/main/java/com/kiselev/reflection/ui/impl/bytecode/assembly/attach/AgentAttacher.java
@@ -1,9 +1,6 @@
 package com.kiselev.reflection.ui.impl.bytecode.assembly.attach;
 
 import com.ea.agentloader.AgentLoader;
-import com.sun.tools.attach.VirtualMachine;
-
-import java.lang.management.ManagementFactory;
 
 /**
  * Created by Vadim Kiselev on 6/13/2017.
