d666d88c9c0cb5cfb988c6c700b1a8b8703d68e9
amakaroff <alekseymakarov1996@gmail.com>
2017-07-14 02:22:18 +0400

    Change location configuration interface

---
+++ src/main/java/com/kiselev/reflection/ui/configuration/Configuration.java
@@ -0,0 +1,11 @@
+package com.kiselev.reflection.ui.configuration;
+
+import java.util.Map;
+
+/**
+ * Created by Aleksei Makarov on 06/24/2017.
+ */
+public interface Configuration {
+
+    Map<String, Object> getConfiguration();
+}
